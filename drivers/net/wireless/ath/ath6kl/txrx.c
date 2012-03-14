a8fb335d2f8de098b037f5ba946bccf132bd085f
Kalle Valo <kvalo@qca.qualcomm.com>
2012-03-14 13:21:42 +0200

    ath6kl: fix indentation
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/txrx.c
+++ drivers/net/wireless/ath/ath6kl/txrx.c
@@ -670,7 +670,7 @@ static void ath6kl_tx_clear_node_map(struct ath6kl_vif *vif,
 }
 
 void ath6kl_tx_complete(struct htc_target *target,
-				struct list_head *packet_queue)
+			struct list_head *packet_queue)
 {
 	struct ath6kl *ar = target->dev->ar;
 	struct sk_buff_head skb_queue;
