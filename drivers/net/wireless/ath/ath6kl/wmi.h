fda163c989c9c4fdde27dab7516779f66caa5269
Thomas Pedersen <c_tpeder@qca.qualcomm.com>
2012-06-21 12:50:08 -0700

    ath6kl: support rssi threshold for sched scan
    The ath6kl firmware can filter scan results based on rssi. This is
    useful to limit hosts wakeups on scheduled scans.
    
    Signed-off-by: Thomas Pedersen <c_tpeder@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>
    (cherry picked from commit 9c805658a34172037c0ce41fc509d2ba3f007314)

--- drivers/net/wireless/ath/ath6kl/wmi.h
+++ drivers/net/wireless/ath/ath6kl/wmi.h
@@ -622,6 +622,10 @@ enum wmi_cmd_id {
 	WMI_SET_MCASTRATE,
 
 	WMI_STA_BMISS_ENHANCE_CMDID,
+
+	WMI_SET_REGDOMAIN_CMDID,
+
+	WMI_SET_RSSI_FILTER_CMDID,
 };
 
 enum wmi_mgmt_frame_type {
@@ -1263,6 +1267,11 @@ struct wmi_snr_threshold_params_cmd {
 	u8 thresh_below4_val;
 
 	u8 reserved[3];
+} __packed;
+
+/* Don't report BSSs with signal (RSSI) below this threshold */
+struct wmi_set_rssi_filter_cmd {
+	s8 rssi;
 } __packed;
 
 enum wmi_preamble_policy {
@@ -2554,6 +2563,7 @@ int ath6kl_wmi_add_wow_pattern_cmd(struct wmi *wmi, u8 if_idx,
 				   const u8 *mask);
 int ath6kl_wmi_del_wow_pattern_cmd(struct wmi *wmi, u8 if_idx,
 				   u16 list_id, u16 filter_id);
+int ath6kl_wmi_set_rssi_filter_cmd(struct wmi *wmi, u8 if_idx, s8 rssi);
 int ath6kl_wmi_set_roam_lrssi_cmd(struct wmi *wmi, u8 lrssi);
 int ath6kl_wmi_force_roam_cmd(struct wmi *wmi, const u8 *bssid);
 int ath6kl_wmi_set_roam_mode_cmd(struct wmi *wmi, enum wmi_roam_mode mode);
